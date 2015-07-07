0d0193c3f1d952ef2083849bbae1b922b3b45ab8
joator22 <joator22@hotmail.com>
2015-07-07 18:03:21 +0200

    ejercicio

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,17 @@
+exports.question = function(req,res){
+  res.render('quizes/question',{pregunta:'Capital de Italia'});  
+};
+
+exports.answer = function(req,res){
+
+    if (req.query.respuesta.toUpperCase() ==='ROMA'){
+        res.render('quizes/answer',{respuesta:'Correcto'});
+    }else{
+        res.render('quizes/answer',{respuesta:'Incorrecto'});
+    }
+};
+
+
+exports.author = function(req,res){
+    res.render('quizes/author',{autor:'Joaquin Torralba Barranco'});      
+   };
